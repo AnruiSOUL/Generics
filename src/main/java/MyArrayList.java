800ca839c37ca50905eb8a3b8aa9ccc3a96f6fcb
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-02-22 15:15:44 -0500

    stubbed of he enigine

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,5 +1,58 @@
 /**
  * Created by randallcrame on 2/22/17.
  */
-public class MyArrayList {
+public class MyArrayList<T> {
+//    +add(E): boolean
+//+add(int, E): void
+//+clear(): void
+//+get(int)abstract E
+//+ indexOf(Object): int
+//    remove(int): E
+//    set(int, E): E
+
+    Object[] array;
+
+    MyArrayList(){
+    array = new Object[10];
+    }
+
+    MyArrayList(int size){
+    array = new Object[size];
+    }
+
+    public int size(){
+        return 0;
+    }
+
+
+    public boolean add(){
+        return false;
+    }
+
+    public void add(T element, int index ){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public <T> T get(int index){
+        return null;
+    }
+
+    public int indexOf(T){
+        return 0;
+    }
+
+    public <T> T remove(int){
+
+        return null;
+    }
+
+    public <T> T set(int index, T value){
+        return null;
+    }
+
+
 }
--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -1,12 +1,22 @@
 import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
 /**
  * Created by randallcrame on 2/22/17.
  */
 public class MyArrayListTest {
-
+    MyArrayList testList, testList2;
     @Before
     public void setUp(){
+        testList = new MyArrayList();
+        testList2 = new MyArrayList(20);
     }
 
+    @Test
+    public void myArrayListSizeTest(){
+        int expected = 10;
+        int actual =  testList.size();
+        assertEquals("Expect a return of 10", expected, actual);
+    }
 }
